f69d2af8dffcbf84fc891155719cf84441f78c1b
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 20:57:34 +0100

    Update niveau1.py

--- niveau1.py
+++ niveau1.py
@@ -5,16 +5,12 @@ def niveau1():
     """Premier niveau du jeu"""
     act = True
     
-    # Chargement des éléments spécifiques au niveau 1
+    # Fond niveau 1
     fnd_niv1 = image.load("background/niveau1_bg.png").convert()
     fnd_niv1 = transform.scale(fnd_niv1, (lrg, htr))
     
     while act:
-        # Affichage niveau 1
         ecr.blit(fnd_niv1, (0, 0))
-        
-        # Logique du niveau 1 ici
-        
         display.flip()
         
         for evt in event.get():
@@ -22,5 +18,4 @@ def niveau1():
                 return False
             if evt.type == KEYDOWN and evt.key == K_ESCAPE:
                 act = False
-                
     return True
