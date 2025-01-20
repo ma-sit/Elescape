10762ed40208ecabb928ccae7fe521c14349c88d
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 11:36:16 +0100

    Update parametres.py

--- parametres.py
+++ parametres.py
@@ -1,19 +1,19 @@
-import pygame
+from pygame import *
 from config import *
 
 def page_parametres(ecr, fnt):
-    """Gère la page des paramètres"""
+    """Page des paramètres"""
     act = True
     while act:
         ecr.fill((150, 150, 150))
         txt = fnt.render("Page des Paramètres - Appuyez sur Echap pour revenir", True, BLC)
-        ecr.blit(txt, (50, HTR // 2 - 25))
-        pygame.display.flip()
+        ecr.blit(txt, (50, htr // 2 - 25))
+        display.flip()
 
-        for evt in pygame.event.get():
-            if evt.type == pygame.QUIT:
+        for evt in event.get():
+            if evt.type == QUIT:
                 return False
-            if evt.type == pygame.KEYDOWN and evt.key == pygame.K_ESCAPE:
+            if evt.type == KEYDOWN and evt.key == K_ESCAPE:
                 act = False
     return True
 
