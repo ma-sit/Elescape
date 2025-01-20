b9b10272215014799d95b37aea5892265972a94e
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 11:36:02 +0100

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,19 +1,18 @@
-import pygame
+from pygame import *
 from config import *
 
 def page_jeu(ecr, fnt):
-    """Gère la page de jeu"""
+    """Page du jeu"""
     act = True
     while act:
         ecr.fill((100, 100, 100))
         txt = fnt.render("Page de Jeu - Appuyez sur Echap pour revenir", True, BLC)
-        ecr.blit(txt, (50, HTR // 2 - 25))
-        pygame.display.flip()
+        ecr.blit(txt, (50, htr // 2 - 25))
+        display.flip()
 
-        for evt in pygame.event.get():
-            if evt.type == pygame.QUIT:
+        for evt in event.get():
+            if evt.type == QUIT:
                 return False
-            if evt.type == pygame.KEYDOWN and evt.key == pygame.K_ESCAPE:
+            if evt.type == KEYDOWN and evt.key == K_ESCAPE:
                 act = False
     return True
-
