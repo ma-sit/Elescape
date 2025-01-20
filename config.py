95b7c2a652f32a18d001375936a3585e59bb5b00
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 11:06:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import pygame
+
+# Variables globales pour l'écran
+LRG = 800
+HTR = 600
+
+# Couleurs basiques
+BLC = (255, 255, 255)
+NOR = (0, 0, 0)
+
+# Rectangles des boutons
+btn_jeu = pygame.Rect(300, 150, 200, 50)
+btn_cfg = pygame.Rect(300, 250, 200, 50)
+btn_fin = pygame.Rect(300, 350, 200, 50)
 
