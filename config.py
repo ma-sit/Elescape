0a01f8b2f66ca9f1a15d8fe021685f6f3020e525
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 11:35:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
-import pygame
+from pygame import *
 
-# Variables globales pour l'écran
-LRG = 800
-HTR = 600
+# Initialisation écran
+ecr = display.set_mode((0, 0), FULLSCREEN)
+rec = ecr.get_rect()
+lrg, htr = ecr.get_size()
 
-# Couleurs basiques
+# Dimensions boutons
+btn_l = 250  # Largeur bouton
+btn_h = 60   # Hauteur bouton
+
+# Couleurs
 BLC = (255, 255, 255)
 NOR = (0, 0, 0)
 
-# Rectangles des boutons
-btn_jeu = pygame.Rect(300, 150, 200, 50)
-btn_cfg = pygame.Rect(300, 250, 200, 50)
-btn_fin = pygame.Rect(300, 350, 200, 50)
+# Police
+fnt = font.Font(None, 50)
 
+# Boutons
+btn_jeu = Rect(lrg/2-btn_l/2, htr/2-100, btn_l, btn_h)
+btn_cfg = Rect(lrg/2-btn_l/2, htr/2, btn_l, btn_h)
+btn_fin = Rect(lrg/2-btn_l/2, htr/2+100, btn_l, btn_h)
