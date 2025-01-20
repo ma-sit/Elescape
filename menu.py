e979b0e2a233f9be379f91dede6fc74133b5a777
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-01-20 20:56:54 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,22 +2,20 @@ from pygame import *
 from config import *
 
 def dessiner_menu(ecr, fnd):
-    """Dessine le menu principal avec les boutons"""
-    ecr.blit(fnd, (0, 0))  # Affiche le fond
-
-    # Dessiner les boutons
+    """Affiche le menu principal"""
+    ecr.blit(fnd, (0, 0))
+    
     draw.rect(ecr, NOR, btn_jeu)
     draw.rect(ecr, NOR, btn_cfg)
     draw.rect(ecr, NOR, btn_fin)
-
-    # Ajouter le texte sur les boutons
+    
     txt_jeu = fnt.render("Jouer", True, BLC)
     txt_cfg = fnt.render("Paramètres", True, BLC)
     txt_fin = fnt.render("Quitter", True, BLC)
-
-    ecr.blit(txt_jeu, (btn_jeu.x + 75, btn_jeu.y + btn_h/5))
-    ecr.blit(txt_cfg, (btn_cfg.x + 25, btn_cfg.y + btn_h/5))
-    ecr.blit(txt_fin, (btn_fin.x + 60, btn_fin.y + btn_h/5))
+    
+    ecr.blit(txt_jeu, (btn_jeu.x + 75, btn_jeu.y + 10))
+    ecr.blit(txt_cfg, (btn_cfg.x + 25, btn_cfg.y + 10))
+    ecr.blit(txt_fin, (btn_fin.x + 60, btn_fin.y + 10))
 
 def plein_ecran():
     """Bascule plein écran"""
