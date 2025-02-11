164820e913e2bd355b89f3c97ec397044a8c7ca7
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-11 09:22:31 +0100

    Update config.py

--- sources/shared/components/config.py
+++ sources/shared/components/config.py
@@ -3,13 +3,13 @@ from pygame import *
 # Initialisation
 init()
 mixer.init()
+
 # Configuration écran
 ecr = display.set_mode((0, 0), FULLSCREEN)
 rec = ecr.get_rect()
 lrg, htr = ecr.get_size()
 
-#Sons des clic
-
+# Sons
 try:
     son_clic = mixer.Sound("data/sons/click_002.wav")
     mixer.music.load("data/sons/musique_test.mp3")
@@ -17,7 +17,6 @@ try:
     mixer.music.play(-1)
 except:
     print("Error : fichiers sons non trouvés")
-    
 
 # Boutons menu
 btn_jeu = Rect(lrg/2-125, htr/2-100, 250, 60)
@@ -34,16 +33,10 @@ fnt = font.Font(None, 50)
 police_titre = font.Font(None, 80)
 police_options = font.Font(None, 40)
 
-# Son
-mixer.music.load("data/sons/musique_test.mp3")
-mixer.music.set_volume(0.5)
-mixer.music.play(-1)
-
 # Position de la barre de volume
 barre_x = 200
-barre_y = 300
+barre_y_general = 250
+barre_y_musique = 350
+barre_y_sfx = 450
 barre_largeur = 400
 barre_hauteur = 20
-
-# Police
-fnt = font.Font(None, 50)
