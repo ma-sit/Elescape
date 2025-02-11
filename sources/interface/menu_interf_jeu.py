10ccbfe55080bdc08f35ac42fde3872a836bec2d
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-11 09:11:51 +0100

    Update menu_interf_jeu.py

--- sources/interface/menu_interf_jeu.py
+++ sources/interface/menu_interf_jeu.py
@@ -1,14 +1,20 @@
 def menu_parametres():
     """Menu des paramètres qui s'affiche par-dessus le niveau"""
+    try:
+        with open("data/touches.json", "r") as f:
+            touches = json.load(f)
+    except:
+        touches = {
+            'Déplacement': BUTTON_RIGHT,
+            'Action': K_SPACE,
+            'Retour': K_ESCAPE
+        }
+        
     menu_actif = True
-    
-    # Dimensions du menu
     menu_width = 300
     menu_height = 200
     menu_x = (lrg - menu_width) // 2
     menu_y = (htr - menu_height) // 2
-    
-    # Création du bouton Quit
     button_width = 120
     button_height = 40
     button_x = menu_x + (menu_width - button_width) // 2
@@ -16,21 +22,17 @@ def menu_parametres():
     quit_button = Rect(button_x, button_y, button_width, button_height)
     
     while menu_actif:
-        # Fond semi-transparent
         surf_overlay = Surface((lrg, htr), SRCALPHA)
         surf_overlay.fill((0, 0, 0, 128))
         ecr.blit(surf_overlay, (0, 0))
         
-        # Dessin du menu
         draw.rect(ecr, (50, 50, 50), (menu_x, menu_y, menu_width, menu_height))
         draw.rect(ecr, (200, 200, 200), (menu_x, menu_y, menu_width, menu_height), 2)
         
-        # Titre du menu
         titre = font.Font(None, 36).render("Parameters", True, BLC)
         titre_rect = titre.get_rect(center=(menu_x + menu_width//2, menu_y + 30))
         ecr.blit(titre, titre_rect)
         
-        # Bouton Quit
         draw.rect(ecr, (150, 150, 150), quit_button)
         draw.rect(ecr, (200, 200, 200), quit_button, 2)
         quit_text = font.Font(None, 32).render("Quit", True, BLC)
@@ -42,10 +44,10 @@ def menu_parametres():
         for evt in event.get():
             if evt.type == QUIT:
                 return False
-            if evt.type == KEYDOWN and evt.key == K_ESCAPE:
+            if evt.type == KEYDOWN and evt.key == touches['Retour']:
                 menu_actif = False
             if evt.type == MOUSEBUTTONDOWN and evt.button == 1:
                 if quit_button.collidepoint(evt.pos):
                     return False
-    
+                    
     return True
