6ca39a981b06352451ac9e8691fa1bd097dc0824
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-11 09:11:14 +0100

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -3,10 +3,30 @@ import sys
 from shared.components.config import *
 from interface.menu import dessiner_menu, plein_ecran
 from interface.selection_niveau import selection_niveau
-from interface.parametres import page_parametres, charger_touches
+from interface.parametres import page_parametres
+import json
+
+# Initialisation
+init()
 
 # Chargement des touches
-touches = charger_touches()
+try:
+    with open("data/touches.json", "r") as f:
+        touches = json.load(f)
+except:
+    touches = {
+        'Déplacement': BUTTON_RIGHT,
+        'Action': K_SPACE,
+        'Retour': K_ESCAPE,
+        'Plein écran': K_f,
+        'Paramètres': K_s,
+        'Jouer': K_p,
+        'Quitter': K_q
+    }
+
+# Chargement fond menu
+fnd = image.load("data/images/bg_menu.png").convert()
+fnd = transform.scale(fnd, (rec.right, rec.bottom))
 
 # Boucle principale
 act = True
@@ -33,3 +53,6 @@ while act:
                 act = page_parametres()
             elif evt.key == touches['Quitter']:
                 act = False
+
+quit()
+sys.exit()
