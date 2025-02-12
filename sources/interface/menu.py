08c23aaf33f626dd31d09645c0f08f9e3ad69a69
simon <simonbille15@gmail.com>
2025-02-12 19:09:51 +0100

    Update menu.py

--- sources/interface/menu.py
+++ sources/interface/menu.py
@@ -4,26 +4,40 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from shared.components.config import *
 
+def bouton(ecr, couleur, btn, texte, son_survol, son_click, surbrillance=None):
+    """Dessine un bouton interactif avec sons et coins arrondis"""
+    rect = btn["rect"]  # Récupérer le pygame.Rect
+    survole = rect.collidepoint(mouse.get_pos())
+
+    if survole:
+        couleur = surbrillance if surbrillance else couleur
+        if not btn["a_joue_son"]:  # Vérifie si le son n'a pas encore été joué
+            son_survol.play()
+            btn["a_joue_son"] = True
+    else:
+        btn["a_joue_son"] = False  # Réinitialise quand la souris quitte le bouton
+
+    draw.rect(ecr, couleur, rect, border_radius=15)  # Dessiner le bouton arrondi
+    txt_rendu = fnt.render(texte, True, (255, 255, 255))
+    ecr.blit(txt_rendu, (rect.x + (rect.width - txt_rendu.get_width()) // 2, rect.y + 10))
+
+    return survole
+    
+
 def dessiner_menu(ecr, fnd):
     """Affiche le menu principal"""
     ecr.blit(fnd, (0, 0))
     
-    draw.rect(ecr, NOR, btn_jeu)
-    draw.rect(ecr, NOR, btn_cfg)
-    draw.rect(ecr, NOR, btn_fin)
+    hover_jeu = bouton(ecr, NOR, btn_jeu, "Jouer", son_survol, son_clicmenu, surbrillance=(150, 150, 150))
+    hover_cfg = bouton(ecr, NOR, btn_cfg, "Paramètres", son_survol, son_clicmenu, surbrillance=(150, 150, 150))
+    hover_fin = bouton(ecr, NOR, btn_fin, "Quitter", son_survol, son_clicmenu, surbrillance=(150, 150, 150))
     
     """Titre du jeu"""
-    y=200
-    txt_texte = police_titre.render("Vitanox", True, (255, 255, 255))
-    rect_texte = txt_texte.get_rect(center=(lrg // 2, y))
+    y=300
+    txt_titre = police_titre.render("Vitanox", True, (0, 0, 0))
+    rect_titre = txt_titre.get_rect(center=(lrg // 2, y))
+    ecr.blit(txt_titre, rect_titre)
     
-    txt_jeu = fnt.render("Jouer", True, BLC)
-    txt_cfg = fnt.render("Paramètres", True, BLC)
-    txt_fin = fnt.render("Quitter", True, BLC)
-    
-    ecr.blit(txt_jeu, (btn_jeu.x + 75, btn_jeu.y + 10))
-    ecr.blit(txt_cfg, (btn_cfg.x + 25, btn_cfg.y + 10))
-    ecr.blit(txt_fin, (btn_fin.x + 60, btn_fin.y + 10))
 
 def plein_ecran():
     """Bascule plein écran"""
