b61e9d21e631a94c1e025e77a95737986f6fb5c8
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-18 09:27:28 +0100

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -1,69 +1,46 @@
-from pygame import *
 import sys
 import json
 from shared.components.config import *
-from interface.menu import dessiner_menu
+from interface.menu import bouton, dessiner_menu, plein_ecran
+from interface.menu import bouton,dessiner_menu, plein_ecran
 from interface.selection_niveau import selection_niveau
 from interface.parametres import page_parametres
 
-# Initialisation
-init()
-
-# Charger ou réinitialiser les touches utilisateur
-def charger_touches():
-    try:
-        with open("data/touches.json", "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {
-            'Déplacement': BUTTON_RIGHT,
-            'Action': K_SPACE,
-            'Retour': K_ESCAPE,
-            'Plein écran': K_f,
-            'Paramètres': K_s,
-            'Jouer': K_p,
-            'Quitter': K_q
-        }
-
-# Sauvegarder les touches dans le fichier
-def sauvegarder_touches(touches):
-    with open("data/touches.json", "w") as f:
-        json.dump(touches, f)
-
-touches = charger_touches()
+@@ -24,6 +24,41 @@
+        'Quitter': K_q
+    }
 
 # Boucle principale
-def boucle_principale():
-    act = True
-    while act:
-        dessiner_menu(ecr)
-        
-        for evt in event.get():
-            if evt.type == QUIT:
+act = True
+while act:
+    dessiner_menu(ecr)
+    
+    for evt in event.get():
+        if evt.type == QUIT:
+            act = False
+        if evt.type == MOUSEBUTTONDOWN and evt.button == 1:
+            if btn_jeu["rect"].collidepoint(evt.pos):
+                son_clicmenu.play()
+                act = selection_niveau()
+            elif btn_cfg["rect"].collidepoint(evt.pos):
+                son_clicmenu.play()
+                act = page_parametres()
+            elif btn_fin["rect"].collidepoint(evt.pos):
+                son_clicmenu.play()
                 act = False
-            elif evt.type == MOUSEBUTTONDOWN and evt.button == 1:
-                if btn_jeu["rect"].collidepoint(evt.pos):
-                    son_clicmenu.play()
-                    act = selection_niveau()
-                elif btn_cfg["rect"].collidepoint(evt.pos):
-                    son_clicmenu.play()
-                    act = page_parametres()
-                elif btn_fin["rect"].collidepoint(evt.pos):
-                    son_clicmenu.play()
-                    act = False
-            elif evt.type == KEYDOWN:
-                if evt.key == touches['Jouer']:
-                    act = selection_niveau()
-                elif evt.key == touches['Paramètres']:
-                    act = page_parametres()
-                elif evt.key == touches['Quitter']:
-                    act = False
-        
-        display.flip()
-        time.delay(30)
-
-    quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    boucle_principale()
+        if evt.type == KEYDOWN:
+            if evt.key == touches['Plein écran']:
+                plein_ecran()
+            elif evt.key == touches['Jouer']:
+                act = selection_niveau()
+            elif evt.key == touches['Paramètres']:
+                act = page_parametres()
+            elif evt.key == touches['Quitter']:
+                act = False
+    display.flip()
+    time.delay(30)
+quit()
+sys.exit()
+# Boucle principale
+act = True
+while act:
