9325cd900263e9326356267896a5ee54458447e6
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-18 08:56:33 +0100

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -2,7 +2,7 @@ from pygame import *
 import sys
 import json
 from shared.components.config import *
-from interface.menu import bouton,dessiner_menu, plein_ecran
+from interface.menu import bouton, dessiner_menu, plein_ecran
 from interface.selection_niveau import selection_niveau
 from interface.parametres import page_parametres
 
@@ -24,12 +24,11 @@ except:
         'Quitter': K_q
     }
 
-
 # Boucle principale
 act = True
 while act:
     dessiner_menu(ecr)
-    
+
     for evt in event.get():
         if evt.type == QUIT:
             act = False
@@ -44,9 +43,7 @@ while act:
                 son_clicmenu.play()
                 act = False
         if evt.type == KEYDOWN:
-            if evt.key == touches['Plein écran']:
-                plein_ecran()
-            elif evt.key == touches['Jouer']:
+            if evt.key == touches['Jouer']:
                 act = selection_niveau()
             elif evt.key == touches['Paramètres']:
                 act = page_parametres()
@@ -57,4 +54,3 @@ while act:
 
 quit()
 sys.exit()
-
