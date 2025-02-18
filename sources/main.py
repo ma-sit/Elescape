d92ad85bc620209b1d7ad3a898acb0e659351c43
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-18 09:12:29 +0100

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -1,34 +1,19 @@
-from pygame import *
 import sys
 import json
 from shared.components.config import *
-from interface.menu import bouton, dessiner_menu, plein_ecran
+from interface.menu import bouton,dessiner_menu, plein_ecran
 from interface.selection_niveau import selection_niveau
 from interface.parametres import page_parametres
 
-# Initialisation
-init()
-
-# Chargement des touches
-try:
-    with open("data/touches.json", "r") as f:
-        touches = json.load(f)
-except:
-    touches = {
-        'Déplacement': BUTTON_RIGHT,
-        'Action': K_SPACE,
-        'Retour': K_ESCAPE,
-        'Plein écran': K_f,
-        'Paramètres': K_s,
-        'Jouer': K_p,
+@@ -24,6 +24,41 @@
         'Quitter': K_q
     }
 
 # Boucle principale
 act = True
 while act:
-    dessiner_menu(ecr)  # Affiche le menu principal
-
+    dessiner_menu(ecr)
+    
     for evt in event.get():
         if evt.type == QUIT:
             act = False
@@ -43,7 +28,9 @@ while act:
                 son_clicmenu.play()
                 act = False
         if evt.type == KEYDOWN:
-            if evt.key == touches['Jouer']:
+            if evt.key == touches['Plein écran']:
+                plein_ecran()
+            elif evt.key == touches['Jouer']:
                 act = selection_niveau()
             elif evt.key == touches['Paramètres']:
                 act = page_parametres()
@@ -51,6 +38,11 @@ while act:
                 act = False
     display.flip()
     time.delay(30)
-
 quit()
 sys.exit()
+# Boucle principale
+act = True
+while act:
+0 commit comments
+Comments
+0
