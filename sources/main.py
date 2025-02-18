fca12069cee36a157d4ce556b4bcfea12f0a2afb
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-02-18 09:10:44 +0100

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -2,7 +2,7 @@ from pygame import *
 import sys
 import json
 from shared.components.config import *
-from interface.menu import bouton,dessiner_menu, plein_ecran
+from interface.menu import bouton, dessiner_menu, plein_ecran
 from interface.selection_niveau import selection_niveau
 from interface.parametres import page_parametres
 
@@ -24,45 +24,10 @@ except:
         'Quitter': K_q
     }
 
-
 # Boucle principale
 act = True
 while act:
-    dessiner_menu(ecr)
-    
-    for evt in event.get():
-        if evt.type == QUIT:
-            act = False
-        if evt.type == MOUSEBUTTONDOWN and evt.button == 1:
-            if btn_jeu["rect"].collidepoint(evt.pos):
-                son_clicmenu.play()
-                act = selection_niveau()
-            elif btn_cfg["rect"].collidepoint(evt.pos):
-                son_clicmenu.play()
-                act = page_parametres()
-            elif btn_fin["rect"].collidepoint(evt.pos):
-                son_clicmenu.play()
-                act = False
-        if evt.type == KEYDOWN:
-            if evt.key == touches['Plein écran']:
-                plein_ecran()
-            elif evt.key == touches['Jouer']:
-                act = selection_niveau()
-            elif evt.key == touches['Paramètres']:
-                act = page_parametres()
-            elif evt.key == touches['Quitter']:
-                act = False
-    display.flip()
-    time.delay(30)
-
-quit()
-sys.exit()
-
-
-# Boucle principale
-act = True
-while act:
-    dessiner_menu(ecr)
+    dessiner_menu(ecr)  # Affiche le menu principal
 
     for evt in event.get():
         if evt.type == QUIT:
