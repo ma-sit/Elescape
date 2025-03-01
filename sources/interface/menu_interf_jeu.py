3a6f8b7145f20043e2930f9815867ba93a0a5cf4
ma-sit <158492021+ma-sit@users.noreply.github.com>
2025-03-01 20:28:02 +0100

    Update menu_interf_jeu.py

--- sources/interface/menu_interf_jeu.py
+++ sources/interface/menu_interf_jeu.py
@@ -1,6 +1,7 @@
 from pygame import *
 import json
 from shared.components.config import *
+from interface.menu import bouton
 
 def menu_parametres():
     """Menu des paramètres qui s'affiche par-dessus le niveau"""
@@ -22,25 +23,30 @@ def menu_parametres():
     button_height = 40
     button_x = menu_x + (menu_width - button_width) // 2
     button_y = menu_y + menu_height - button_height - 20
-    quit_button = Rect(button_x, button_y, button_width, button_height)
+    
+    # Bouton quitter avec le format compatible avec la fonction bouton()
+    quit_button = {
+        "rect": Rect(button_x, button_y, button_width, button_height),
+        "image": None,
+        "a_joue_son": False,
+        "text": "Quit"
+    }
     
     while menu_actif:
         surf_overlay = Surface((lrg, htr), SRCALPHA)
         surf_overlay.fill((0, 0, 0, 128))
         ecr.blit(surf_overlay, (0, 0))
         
-        draw.rect(ecr, (50, 50, 50), (menu_x, menu_y, menu_width, menu_height))
-        draw.rect(ecr, BLC, (menu_x, menu_y, menu_width, menu_height), 2)
+        # Rectangle de fond du menu avec coins arrondis
+        draw.rect(ecr, (50, 50, 50), (menu_x, menu_y, menu_width, menu_height), border_radius=15)
+        draw.rect(ecr, BLC, (menu_x, menu_y, menu_width, menu_height), 2, border_radius=15)
         
         titre = font.Font(None, 36).render("Parameters", True, BLC)
         titre_rect = titre.get_rect(center=(menu_x + menu_width//2, menu_y + 30))
         ecr.blit(titre, titre_rect)
         
-        draw.rect(ecr, (150, 150, 150), quit_button)
-        draw.rect(ecr, BLC, quit_button, 2)
-        quit_text = font.Font(None, 32).render("Quit", True, BLC)
-        quit_text_rect = quit_text.get_rect(center=quit_button.center)
-        ecr.blit(quit_text, quit_text_rect)
+        # Utilisation de la fonction bouton avec arrondis
+        hover = bouton(ecr, (150, 150, 150), quit_button, quit_button["text"], son_survol, son_clicmenu, 10, surbrillance=BLC)
         
         display.flip()
         
@@ -50,7 +56,8 @@ def menu_parametres():
             if evt.type == KEYDOWN and evt.key == touches['Retour']:
                 menu_actif = False
             if evt.type == MOUSEBUTTONDOWN and evt.button == 1:
-                if quit_button.collidepoint(evt.pos):
+                if quit_button["rect"].collidepoint(evt.pos):
+                    son_clicmenu.play()
                     return False
     
     return True
